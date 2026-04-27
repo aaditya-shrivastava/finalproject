1b882f63361e649f004f96de82f5c5eaba753155
aaditya-shrivastava <aadityashrivastava2004@gmail.com>
2026-04-27 10:28:58 +0530

    updating the .env settings

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-65.0.232.208:8080/main-app/
+http://65.0.232.208:8080/main-app/
+http://65.0.232.208:8080/manufacturing-creator/
+http://65.0.232.208:8080/multi-agent-manufacturing/
--- multi-agent-manufacturing/config.py
+++ multi-agent-manufacturing/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 def get_llm():
     return LLM(
         model="groq/llama-3.3-70b-versatile",   # ✅ CrewAI format: provider/model
-        api_key="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", # ✅ Your API key from CrewAI
+        api_key="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", # ✅ Your API key from CrewAI
         temperature=0.3,
         max_tokens=4096
     )
