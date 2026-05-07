e8123031b2a96b625fcf51592f528c865c968a26
aaditya-shrivastava <aadityashrivastava2004@gmail.com>
2026-05-07 10:48:24 +0530

    updating k8s folder

--- multi-agent-manufacturing/config.py
+++ multi-agent-manufacturing/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 def get_llm():
     return LLM(
         model="groq/llama-3.3-70b-versatile",   # ✅ CrewAI format: provider/model
-        api_key=os.getenv("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"), # ✅ Your API key from CrewAI
+        api_key=os.getenv("GROQ_API_KEY"), # ✅ Your API key from CrewAI
         temperature=0.3,
         max_tokens=4096
     )
